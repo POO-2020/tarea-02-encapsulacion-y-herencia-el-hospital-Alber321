86c98186274e56702da8895b0964a7f09d1df7dd
Alber321 <lsolis2@ucol.mx>
2020-03-22 17:35:20 -0600

    Avance

---
+++ cita.js
@@ -0,0 +1,16 @@
+export default class Cita {
+    constructor(fecha, hora, doctor, paciente){
+        this._fecha = fecha
+        this._hora = hora
+        this._doctor = doctor
+        this._paciente = paciente
+    }
+    getCita(){
+        return (`${this._fecha.getFecha()} ${this._hora.getFormato24()}
+         ${this._doctor._nombre.getNombreCompleto()} ${this._paciente.getPerfil()}`)
+    }
+    _esIgualA(cita){
+        if(cita == this){ return true}
+        else {return false}
+    }
+}
---
+++ doctor.js
@@ -0,0 +1,28 @@
+export default class Doctor{
+
+    constructor({nombre, especialidad, telefono, cedula})
+    {
+        this._nombre=nombre
+        this._especialidad=especialidad
+        this._telefono=telefono
+        this._cedula=cedula
+    }
+
+    getPerfil()
+    {
+        return (`${this._cedula}, ${this._especialidad}, ${this._nombre.getNombreCompleto()}, ${this._telefono}`)
+    }
+
+    getApellidoPaterno(){
+        return this._nombre.getApellidoPaterno()
+    }
+
+    _esIgualA(doc){
+        if(doc._getCedula() == this._cedula){ return true}
+        else {return false}
+    }
+
+    _getCedula(){
+        return this._cedula
+    }
+}
